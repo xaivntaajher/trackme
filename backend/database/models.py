80259a162ce659dff8a4fec052715dbe23f2fbe6
xaivntaajher <dn_her@yahoo.com>
2023-07-27 14:52:20 -0500

    create model for backend

--- backend/database/models.py
+++ backend/database/models.py
@@ -3,13 +3,30 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
+
+
+user_event = db.Table('user_event',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True), 
+    db.Column('event_id', db.Integer, db.ForeignKey('event.id'), primary_key=True)
+)
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False)
     first_name = db.Column(db.String(255), nullable=False)
     last_name = db.Column(db.String(255), nullable=False)
-    email = db.Column(db.String(255), nullable=False, unique=True)
+    is_coach = db.Column(db.Boolean, default=False)
+    pin = db.Column(db.String(4))
+    start_date = db.Column(db.Date)
+    last_promotion = db.Column(db.Date)
+    point_total = db.Column(db.Integer, default=0)
+    rank_id = db.Column(db.Integer, db.ForeignKey('rank.id'))
+    rank = db.relationship("Rank")
+
+    events = db.relationship('Event', secondary=user_event, backref=db.backref('users', lazy='dynamic'))
+    # promotions = db.relationship('Rank', secondary=promotion, backref=db.backref('promoted_users', lazy='dynamic'))
+    promotions = db.relationship('Promotion')
 
     def hash_password(self):
         self.password = generate_password_hash(self.password).decode('utf8')
@@ -31,3 +48,24 @@ class Car(db.Model):
     user = db.relationship("User")
 
 # TODO: Add your models below, remember to add a new migration and upgrade database
+
+class Event(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    points = db.Column(db.Integer, nullable=False)
+    title = db.Column(db.String(255), nullable=False)
+    type = db.Column(db.String(255), nullable=False)
+    date = db.Column(db.Date)
+    capacity = db.Column(db.Integer)
+
+class Rank(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    points_required = db.Column(db.Integer)
+    title = db.Column(db.String(255), nullable=False)
+    is_child_rank = db.Column(db.Boolean)
+
+class Promotion(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    date = db.Column(db.Date)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    rank_id = db.Column(db.Integer, db.ForeignKey('rank.id'))
+    rank = db.relationship("Rank")
